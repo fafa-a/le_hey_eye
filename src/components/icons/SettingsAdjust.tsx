4137fb6ae5b61f686835d16cf68378de64788014
fafa <fabienpuech1@gmail.com>
2025-02-05 22:37:58 +0100

    feat(icons): add side panel icons and improve accessibility
    - Add SidePanelOpen and SidePanelClose icon components
    - Add SVG title elements for better accessibility
    - Update imports to use explicit type imports from solid-js

--- src/components/icons/SettingsAdjust.tsx
+++ src/components/icons/SettingsAdjust.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "solid-js";
+import type { JSX } from "solid-js";
 export default function CarbonSettingsAdjust(
 	props: JSX.IntrinsicElements["svg"],
 ) {
@@ -10,6 +10,7 @@ export default function CarbonSettingsAdjust(
 			viewBox="0 0 32 32"
 			{...props}
 		>
+			<title>Settings Adjust</title>
 			<path
 				fill="currentColor"
 				d="M30 8h-4.1c-.5-2.3-2.5-4-4.9-4s-4.4 1.7-4.9 4H2v2h14.1c.5 2.3 2.5 4 4.9 4s4.4-1.7 4.9-4H30zm-9 4c-1.7 0-3-1.3-3-3s1.3-3 3-3s3 1.3 3 3s-1.3 3-3 3M2 24h4.1c.5 2.3 2.5 4 4.9 4s4.4-1.7 4.9-4H30v-2H15.9c-.5-2.3-2.5-4-4.9-4s-4.4 1.7-4.9 4H2zm9-4c1.7 0 3 1.3 3 3s-1.3 3-3 3s-3-1.3-3-3s1.3-3 3-3"
