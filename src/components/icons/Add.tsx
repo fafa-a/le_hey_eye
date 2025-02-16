98026897ec64e9440d1b6197c22df6b3e44084a1
fafa <fabienpuech1@gmail.com>
2025-02-16 01:20:22 +0100

    feat(components): add tooltip and icon components
    Add new UI components:
    - Tooltip component with content and trigger subcomponents

---
+++ src/components/icons/Add.tsx
@@ -0,0 +1,17 @@
+import type { JSX } from "solid-js";
+
+export function CarbonAdd(props: JSX.IntrinsicElements["svg"]) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="1em"
+			height="1em"
+			viewBox="0 0 32 32"
+			{...props}
+		>
+			<title>Add</title>
+			<path fill="currentColor" d="M17 15V8h-2v7H8v2h7v7h2v-7h7v-2z" />
+		</svg>
+	);
+}
+export default CarbonAdd;
---
+++ src/components/icons/ThreeDots.tsx
@@ -0,0 +1,20 @@
+import type { JSX } from "solid-js";
+
+export function PhDotsThreeVerticalBold(props: JSX.IntrinsicElements["svg"]) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="1em"
+			height="1em"
+			viewBox="0 0 256 256"
+			{...props}
+		>
+			<title>Three Dots Vertical</title>
+			<path
+				fill="currentColor"
+				d="M112 60a16 16 0 1 1 16 16a16 16 0 0 1-16-16m16 52a16 16 0 1 0 16 16a16 16 0 0 0-16-16m0 68a16 16 0 1 0 16 16a16 16 0 0 0-16-16"
+			/>
+		</svg>
+	);
+}
+export default PhDotsThreeVerticalBold;
